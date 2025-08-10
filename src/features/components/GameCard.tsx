831279e37bc81134a2737df5207164aa18aa832d
Jan Lasota <jlasota@teamworks.com>
2025-08-10 17:11:34 -0500

    testing

--- src/features/components/GameCard.tsx
+++ src/features/components/GameCard.tsx
@@ -185,8 +185,6 @@ const GameCard = ({ dates }: { dates: GameDate[] }) => {
           (team) => team.id === homeTeamId
         );
 
-        // update
-
         return (
           <div
             key={game.gamePk}
